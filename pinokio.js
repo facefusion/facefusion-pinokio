95e38628f1431ad68bc2ca8e98ba79837f96f722
henryruhs <info@henryruhs.com>
2023-12-20 23:13:53 +0100

    Bump to 2.1.1

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,6 @@
 module.exports =
 {
-	title: 'FaceFusion 2.1.0',
+	title: 'FaceFusion 2.1.1',
 	description: 'Next generation face swapper and enhancer',
 	icon: 'icon.png',
 	menu: require(__dirname + '/menu.js')
