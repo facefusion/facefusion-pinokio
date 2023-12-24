a1ab289b92a2df579aecb4d16c288b04d41502e2
henryruhs <info@henryruhs.com>
2023-12-24 16:07:45 +0100

    Bump to 2.1.2

--- update.js
+++ update.js
@@ -15,7 +15,7 @@ module.exports = () =>
 				method: 'shell.run',
 				params:
 				{
-					message: 'git pull origin 2.1.1',
+					message: 'git pull origin 2.1.2',
 					path: 'facefusion'
 				}
 			}
