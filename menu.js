1bb08e6b57fcb108113b44c5e6cdb9aa7a05fb35
henryruhs <info@henryruhs.com>
2024-04-09 19:34:31 +0200

    Okay to name it facefusion as of local scope

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = async kernel =>
 {
-	const hasInstall = await kernel.exists(__dirname, 'facefusion');
+	const hasInstall = await kernel.exists(__dirname, 'facefusion') && !await kernel.running(__dirname, 'install.js')
 
 	let menu = [];
 
