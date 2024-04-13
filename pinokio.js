b01d7f4f19670c03579965c649f40eb45da6b242
henryruhs <info@henryruhs.com>
2024-04-13 12:16:13 +0200

    Update to 2.5.1

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 module.exports =
 {
-	version: '1.4',
-	title: 'FaceFusion 2.5.0',
+	version: '1.5',
+	title: 'FaceFusion 2.5.1',
 	description: 'Next generation face swapper and enhancer',
 	icon: 'icon.png',
 	menu: require(__dirname + '/menu.js')
