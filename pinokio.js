7c2b0778eb3c22bac307379f0d4cf6e540957b3e
henryruhs <info@henryruhs.com>
2024-06-16 21:32:01 +0200

    Bump version 2.6.1

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 module.exports =
 {
 	version: '1.5',
-	title: 'FaceFusion 2.6.0',
+	title: 'FaceFusion 2.6.1',
 	description: 'Next generation face swapper and enhancer',
 	icon: 'icon.png',
 	menu: require(__dirname + '/menu.js')
