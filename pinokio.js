f6608d83cbb2c3fdb64c6f1724e7b7a57844fe44
henryruhs <info@henryruhs.com>
2026-03-08 16:26:37 +0100

    bump version

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 module.exports =
 {
 	version: '1.6',
-	title: 'FaceFusion 3.5.3',
+	title: 'FaceFusion 3.5.4',
 	description: 'Industry leading face manipulation platform',
 	icon: 'facefusion.png',
 	menu: require(__dirname + '/menu.js')
