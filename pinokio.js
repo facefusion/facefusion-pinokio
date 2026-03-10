db25465421a4d7631acff8768a281c5833407f58
henryruhs <info@henryruhs.com>
2026-03-10 11:22:54 +0100

    turns out pinokio.json is eol

--- pinokio.js
+++ pinokio.js
@@ -1,5 +1,8 @@
 module.exports =
 {
 	version: '5.0',
+	title: 'FaceFusion 3.5.4',
+	description: 'Industry leading face manipulation platform',
+	icon: 'facefusion.png',
 	menu: require(__dirname + '/menu.js')
 };
